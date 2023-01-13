301b9dbb42af3d73bb43fcb61eae3eac32f6c74c
Agustin <87285884+AgustinBertero@users.noreply.github.com>
2023-01-13 18:02:11 -0300

    Listing projects of each user

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,7 +1,19 @@
 
 <?php include_once __DIR__ . '/header-dashboard.php'; ?>
 
-<?php debuguear($proyectos); ?>
+<?php if (count($proyectos) === 0) { ?>
+    <p class="no-proyectos"> No projects <a href="/crear-proyecto">Create</a></p>
 
+<?php } else { ?>
+    <ul class="listado-proyectos">
+        <?php foreach($proyectos as $proyecto){  ?>
+                <li class="proyecto">
+                     <a href="/proyecto?id=<?php echo $proyecto->url;?>"> <!-- Redireccion dinamica a cada proyecto -->
+                        <?php echo $proyecto->proyecto; ?>
+                    </a>
+                </li>
+        <?php } ?>
+    </ul>
+<?php } ?>
 
 <?php include_once __DIR__ . '/footer-dashboard.php'; ?>
