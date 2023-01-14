c366c0c72cb6d5b9ae4cc527d4558413ad83fdcd
Agustin <87285884+AgustinBertero@users.noreply.github.com>
2023-01-14 06:00:39 -0300

    Button to add new task

--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -1,7 +1,15 @@
 
 <?php include_once __DIR__ . '/header-dashboard.php'; ?>
 
-
+<div class="contenedor-sm">
+    <div class="conteneedor-nueva-tarea">
+        <button
+        type="button"
+        class="agregar-tarea"
+        id="agregar-tarea">
+        &#43; New task</button>
+    </div>
+</div>
 
 
 <?php include_once __DIR__ . '/footer-dashboard.php'; ?>
