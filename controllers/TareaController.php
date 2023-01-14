5f6da8b5f8e93a4b3ffa209bf0ad8f47f578ee0d
Agustin <87285884+AgustinBertero@users.noreply.github.com>
2023-01-14 11:11:13 -0300

    Creating routing and controllers for tasks

---
+++ controllers/TareaController.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace Controllers;
+
+class TareaController {
+    public static function index(){
+
+    }
+
+    public static function crear(){
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            # code...
+        }
+    }
+
+    public static function actualizar(){
+        
+    }
+
+    public static function eliminar(){
+        
+    }
+
+}
+
+
+?>
--- public/index.php
+++ public/index.php
@@ -7,6 +7,8 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 use Controllers\LoginController;
 use Controllers\DashboardController;
+use Controllers\TareaController;
+
 $router = new Router();
 
 // Login = Ruta inicial (/)
@@ -41,6 +43,12 @@ $router->get('/crear-proyecto', [DashboardController::class, 'crear_proyecto']);
 $router->post('/crear-proyecto', [DashboardController::class, 'crear_proyecto']);
 $router->get('/proyecto', [DashboardController::class, 'proyecto']);
 $router->get('/perfil', [DashboardController::class, 'perfil']);
+
+//API para las tareas 
+$router->get('/api/tareas', [TareaController::class, 'index']);
+$router->post('/api/tarea', [TareaController::class, 'crear']);
+$router->post('/api/actualizar', [TareaController::class, 'actualizar']);
+$router->post('/api/eliminar', [TareaController::class, 'eliminar']);
 
 
 
