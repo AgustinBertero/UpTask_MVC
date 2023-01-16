7727230f5552b26bcfc5b80fe100e5d0219b2c79
Agustin <87285884+AgustinBertero@users.noreply.github.com>
2023-01-16 10:27:09 -0300

    Hiding Panels for Responsive

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -1,3 +1,15 @@
+
+
+<div class="barra-mobile">
+    <h1>UpTask</h1>
+
+        <div class="menu">
+            <img src="build/img/menu.svg" alt="imagen menu" id="mobile-menu">
+        </div>
+</div>
+
+
+
 <div class="barra">
     <p>Hello <span><?php echo $_SESSION['nombre']; ?></span> </p>
 
